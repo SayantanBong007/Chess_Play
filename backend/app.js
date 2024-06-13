858e19b74a7d79b5512cffcc774c7553f36b0747
Sayantan Gain <sayantangain63@gmail.com>
2024-06-13 15:18:00 +0530

    Added fevicon logo

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,60 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
-const matchRouter = require('./routes/match');
-const { globalErrorHandler } = require('./controller/errorController');
-const AppError = require('./utils/AppError');
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+const matchRouter = require("./routes/match");
+const { globalErrorHandler } = require("./controller/errorController");
+const AppError = require("./utils/AppError");
 
 // Create an instance of Express application
 const app = express();
 app.use(express.json());
 app.use(cors()); // Enable CORS for Express.js
-app.use("/api/v1/match",matchRouter);
+app.use("/api/v1/match", matchRouter);
 
 // Create an HTTP server using the Express app
 const appServer = http.createServer(app);
 
 // Create a socket.io server and configure CORS settings
 const io = socketIO(appServer, {
-    cors: {
-        origin: '*', 
-        methods: ['GET', 'POST'],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 // Socket.io event listeners
-io.on('connection', socket => {
-    // Handle connection request from player
-    socket.on('connection-req-from-player', (matchId,address) => {
-        socket.to(matchId).emit('connection-req-from-player', matchId, address);
-    });
+io.on("connection", (socket) => {
+  // Handle connection request from player
+  socket.on("connection-req-from-player", (matchId, address) => {
+    socket.to(matchId).emit("connection-req-from-player", matchId, address);
+  });
 
-    // Handle accepted connection request
-    socket.on('connection-req-accepted', (matchId,address, connectionReqAccepted) => {
-        socket.to(matchId).emit('connection-req-accepted', matchId, address);
-        connectionReqAccepted()
-    });
+  // Handle accepted connection request
+  socket.on(
+    "connection-req-accepted",
+    (matchId, address, connectionReqAccepted) => {
+      socket.to(matchId).emit("connection-req-accepted", matchId, address);
+      connectionReqAccepted();
+    }
+  );
 
-    // Handle chess piece movement
-    socket.on('chess-piece-moved', (matchId, moveData) => {
-        socket.to(matchId).emit('chess-piece-moved',matchId, moveData);
-    });
+  // Handle chess piece movement
+  socket.on("chess-piece-moved", (matchId, moveData) => {
+    socket.to(matchId).emit("chess-piece-moved", matchId, moveData);
+  });
 
-    // Handle joining a match
-    socket.on('join-match', (matchId, connectWithOpponent)=>{
-        socket.join(matchId);
-        connectWithOpponent()
-    })
+  // Handle joining a match
+  socket.on("join-match", (matchId, connectWithOpponent) => {
+    socket.join(matchId);
+    connectWithOpponent();
+  });
 });
 
 // Middleware to handle undefined routes
 app.all("*", (req, res, next) => {
-    next(new AppError(`this ${req.originalUrl} route not defined`, 404));
-})
+  next(new AppError(`this ${req.originalUrl} route not defined`, 404));
+});
 
 // Global error handler middleware
 app.use(globalErrorHandler);
